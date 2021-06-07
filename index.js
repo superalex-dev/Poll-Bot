47f7957b760d49b0ee779bf254c363d29df5ec06
superalex-dev <naidobrixxjr@gmail.com>
2021-06-07 23:17:49 +0300

    updating

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Client, RichEmbed } = require('discord.js');
+const { Client, RichEmbed, Message } = require('discord.js');
+const token = "ODQ5MTY3NjgxMjgyMjQ0NjA4.YLXPBw.SAUuH4EVyEyw1rCE9x4DGjFm0jY";
 const bot = new Client();
 
-const token = "ODQ5MTY3NjgxMjgyMjQ0NjA4.YLXPBw.cLe9xje6Heg7UiPGh-GLmTXtq5M";
 
 const PREFIX = "p!";
 
@@ -9,4 +9,23 @@ bot.on('ready', () => {
     console.log("This bot is active and ready to go!");
 })
 
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+
+        case "poll":
+            const Embed = new RichEmbed()
+                .setColor(0xFFC300)
+                .setTitle("Inititate poll")
+                .setDescrption("p!poll to initiate a simple yes or no poll");
+            if (!args[1]) {
+                message.channel.send(Embed);
+            }
+
+            let msgArgs = args.slice(1).join(" ");
+
+            break;
+    }
+})
 bot.login(token);
